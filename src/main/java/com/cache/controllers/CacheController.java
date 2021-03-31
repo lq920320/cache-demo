e0434c7fd1c21cacebc78aeb0e4ac506089b6308
泽兔 <hunter321@dingtalk.com>
2021-03-31 09:23:28 +0800

    fix: 调整接口顺序

--- src/main/java/com/cache/controllers/CacheController.java
+++ src/main/java/com/cache/controllers/CacheController.java
@@ -24,26 +24,31 @@ import javax.annotation.Resource;
 public class CacheController {
 
     @Resource
-    private CacheableService cacheableService;
-    @Resource
-    private GuavaCacheService guavaCacheService;
+    private MapCacheService mapCacheService;
     @Resource
     private HutoolCacheService hutoolCacheService;
     @Resource
-    private MapCacheService mapCacheService;
+    private GuavaCacheService guavaCacheService;
     @Resource
     private CaffeineService caffeineService;
+    @Resource
+    private CacheableService cacheableService;
 
-    @ApiOperation("获取 Cacheable 缓存数据")
-    @GetMapping("cacheable")
-    public ResultWrapper<CacheResultVO> getCacheableResult(@ApiParam(name = "empNo", value = "员工编号") @RequestParam(value = "empNo", required = false) String empNo) {
-        return null;
-    }
-
-    @GetMapping("guava")
-    @ApiOperation("获取 guava 缓存")
-    public ResultWrapper<CacheResultVO> getGuavaCacheResult(@ApiParam(name = "empNo", value = "员工编号") @RequestParam(value = "empNo", required = false) String empNo) {
-        return null;
+    @GetMapping("map")
+    @ApiOperation("获取 map 缓存")
+    public ResultWrapper<CacheResultVO> getMapCacheResult(
+            @ApiParam(name = "empNo", value = "员工编号") @RequestParam(value = "empNo", required = false) String empNo,
+            @ApiParam(name = "timeout", value = "是否有过期时间（默认60s）") @RequestParam(value = "timeout", required = false) Boolean timeout
+    ) {
+        CacheResultVO result;
+        if (BooleanUtils.toBoolean(timeout)) {
+            // 有过期时间的 map
+            result = mapCacheService.getEmpByNoWithExpireMap(empNo);
+        } else {
+            // 常规 map
+            result = mapCacheService.getEmpByNo(empNo);
+        }
+        return ResultWrapper.of(result);
     }
 
     @GetMapping("hutool")
@@ -63,26 +68,21 @@ public class CacheController {
         return ResultWrapper.of(result);
     }
 
-    @GetMapping("map")
-    @ApiOperation("获取 map 缓存")
-    public ResultWrapper<CacheResultVO> getMapCacheResult(
-            @ApiParam(name = "empNo", value = "员工编号") @RequestParam(value = "empNo", required = false) String empNo,
-            @ApiParam(name = "timeout", value = "是否有过期时间（默认60s）") @RequestParam(value = "timeout", required = false) Boolean timeout
-    ) {
-        CacheResultVO result;
-        if (BooleanUtils.toBoolean(timeout)) {
-            // 有过期时间的 map
-            result = mapCacheService.getEmpByNoWithExpireMap(empNo);
-        } else {
-            // 常规 map
-            result = mapCacheService.getEmpByNo(empNo);
-        }
-        return ResultWrapper.of(result);
+    @GetMapping("guava")
+    @ApiOperation("获取 guava 缓存")
+    public ResultWrapper<CacheResultVO> getGuavaCacheResult(@ApiParam(name = "empNo", value = "员工编号") @RequestParam(value = "empNo", required = false) String empNo) {
+        return null;
     }
 
     @GetMapping("caffeine")
     @ApiOperation("获取 caffeine 缓存")
     public ResultWrapper<CacheResultVO> getCaffeineCacheResult(@ApiParam(name = "empNo", value = "员工编号") @RequestParam(value = "empNo", required = false) String empNo) {
+        return null;
+    }
+
+    @ApiOperation("获取 Cacheable 缓存数据")
+    @GetMapping("cacheable")
+    public ResultWrapper<CacheResultVO> getCacheableResult(@ApiParam(name = "empNo", value = "员工编号") @RequestParam(value = "empNo", required = false) String empNo) {
         return null;
     }
 }
